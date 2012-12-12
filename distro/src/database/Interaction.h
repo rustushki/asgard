cab5a6e59cc9efb76ec6ff01372469663bca27de
Sean Eubanks <seanmeu@gmail.com>
2012-12-11 23:14:32 -0700

    Added header files for base Interaction class and various Interaction subclasses.

---
+++ distro/src/database/Interaction.h
@@ -0,0 +1,34 @@
+/*****************************************************************************
+ * Copyright (c) 2006 Russ Adams, Sean Eubanks, Asgard Contributors
+ * This file is part of Asgard.
+ * 
+ * Asgard is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Asgard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Asgard; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ ****************************************************************************/
+
+#ifndef INTERACTION_H
+#define INTERACTION_H
+
+class Interaction
+{
+   public:
+      Interaction(int priority);
+      ~Interaction();
+      virtual void setPriority(int priority) = 0;
+      virtual int getPriority() const = 0;
+   private:
+      int priority;
+};
+
+#endif //INTERACTION_H
