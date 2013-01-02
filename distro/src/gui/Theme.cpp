f9cdc34340781c44161ed0c2b4659c9c470d26d3
Russ Adams <russ.adams@gmail.com>
2013-01-01 20:50:44 -0500

    Adding some notes to Theme class.

--- distro/src/gui/Theme.cpp
+++ distro/src/gui/Theme.cpp
@@ -8,6 +8,12 @@ SDL_Surface* buildElement(std::string elementName) {
    // Do something with element name.
 
    // TODO: Real work starts here.
+   //
+
+   // TODO: Optimize Theme by storing each theme element in an std::map.  This
+   // method then looks up the element in tha map rather than rebuilding a new
+   // SDL_Surface.
+   //
 
    // Return SDL_Surface.
    return NULL;
